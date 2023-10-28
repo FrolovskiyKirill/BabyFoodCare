9de4f1d4edefbb0997304e43f4ba2ba0c17dc7f4
Kirill <frolkv@gmail.com>
2023-10-28 16:00:38 +0300

    Initial Commit

---
+++ BabyFoodCare/BabyFoodCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFoodCareApp.swift
+//  BabyFoodCare
+//
+//  Created by Kirill Frolovskiy on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyFoodCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
