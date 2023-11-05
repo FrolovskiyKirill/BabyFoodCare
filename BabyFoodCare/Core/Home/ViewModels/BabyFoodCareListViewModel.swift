766afebb7b6a1475649a88de3677ce3a3edb6182
Kirill <frolkv@gmail.com>
2023-11-05 22:23:50 +0300

    BFC-8 New Network Manager

--- BabyFoodCare/Core/Home/ViewModels/BabyFoodCareListViewModel.swift
+++ BabyFoodCare/Core/Home/ViewModels/BabyFoodCareListViewModel.swift
@@ -8,34 +8,6 @@
 import Foundation
 
 final class BabyFoodCareListViewModel: ObservableObject {
-  @Published var babyFoodCare: [BabyFoodCareModel] = []
 
-  func getBabyFoodCare() {
-    //          isLoading = true
-    NetworkManager.shared.getBabyFoodCares { [self] result in
-      DispatchQueue.main.async {
-        //                  isLoading = false
-        switch result {
-        case .success(let babyFoodCare):
-          self.babyFoodCare = babyFoodCare
-
-        case .failure(let error):
-          //                      switch error {
-          print(error.localizedDescription)
-          //                      case .invalidResponse:
-          //                          alertItem = AlertContext.invalidResponse
-          //
-          //                      case .invalidURL:
-          //                          alertItem = AlertContext.invalidURL
-          //
-          //                      case .invalidData:
-          //                          alertItem = AlertContext.invalidData
-          //
-          //                      case .unableToComplete:
-          //                          alertItem = AlertContext.unableToComplete
-        }
-      }
-    }
-  }
 }
 
