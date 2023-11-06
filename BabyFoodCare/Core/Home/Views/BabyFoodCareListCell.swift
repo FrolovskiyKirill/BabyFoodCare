bb1ff6f9bc53ee94732705c9a3e695aedbbc4900
Kirill <frolkv@gmail.com>
2023-11-06 11:56:22 +0300

    BFC-9 Add image network service

--- BabyFoodCare/Core/Home/Views/BabyFoodCareListCell.swift
+++ BabyFoodCare/Core/Home/Views/BabyFoodCareListCell.swift
@@ -13,28 +13,9 @@ struct BabyFoodCareListCell: View {
   
   var body: some View {
     HStack {
-      Image("papper")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
+      BabyFoodCareImageView(babyFoodCareModel: babyFoodCare)
         .frame(width: 120, height: 90)
         .cornerRadius(8)
-      
-      //          AsyncImage(url: URL(string: appetizer.imageURL)) { image in
-      //              image
-      //                  .resizable()
-      //                  .aspectRatio(contentMode: .fit)
-      //                  .frame(width: 120, height: 90)
-      //        .cornerRadius(8)
-      
-      //  placeholder: {
-      //      Image("food-placeholder")
-      //        .resizable()
-      //        .aspectRatio(contentMode: .fit)
-      //        .frame(width: 120, height: 90)
-      //        .cornerRadius(8)
-      //    }
-      
-      
       VStack(alignment: .leading, spacing: 5) {
         Text(babyFoodCare.name)
           .font(.title2)
@@ -50,5 +31,5 @@ struct BabyFoodCareListCell: View {
 }
 
 #Preview {
-  BabyFoodCareListCell(babyFoodCare: MockData.orderItemOne)
+  BabyFoodCareListCell(babyFoodCare: MockData.sampleBabyFoodCare)
 }
